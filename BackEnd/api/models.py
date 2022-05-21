bf6be1390879a9b7d269faf7afb21389448da7a2
Haideep <raihaideep@gmail.com>
2022-05-21 13:15:16 +0545

    This is my first commit

---
+++ BackEnd/api/models.py
@@ -0,0 +1,115 @@
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name,tc, password=None,password2=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            tc=tc,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name,tc, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            tc=tc,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=200)
+    tc = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','tc']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
+#STARTING THE OTHE MODELS REQUIRED IN THE DATABASE
+
+#THE CUSTOMER MODEL 
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    phone=models.IntegerField()
+    city=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return str(self.id)
+
+#THE CATEGORY MODEL
+class Category(models.Model):
+    
+    category_name=models.CharField(max_length=255)
+    def __str__(self) :
+        return self.category_name
+
+
+#THE PRODUCT MODEL
+class Products(models.Model):
+    product_name=models.CharField(max_length=255)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    price=models.IntegerField()
+    quantity=models.IntegerField(default=1)
+    desc=models.CharField(max_length=255)
+    image=models.ImageField(upload_to='images',default="")
+
+    def __str__(self):
+        return self.product_name
+
+
+
+
+    
---
+++ BackEnd/api/views.py
@@ -0,0 +1,100 @@
+from functools import partial
+from pstats import Stats
+from django.shortcuts import render
+from .models import Products,Category
+from api.serializers import UserLoginSerializer,ProductsSerializers, UserRegistrationSerializer,CategorySerializers
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+class UserRegistrationView(APIView):
+    def post(self,request,format=None):
+        serializer=UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user=serializer.save()
+            token=get_tokens_for_user(user)
+            return Response({'token':token,'msg':'Registration Success'},status=status.HTTP_201_CREATED)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self,request,format=None):
+        serializer=UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email=serializer.data.get('email')
+            password=serializer.data.get('password')
+            user= authenticate(email=email,password=password)
+            if user is not None:
+                token=get_tokens_for_user(user)
+                return Response({'token':token,'msg':'Login Success'},status=status.HTTP_200_OK)
+            else:
+                 return Response({'errors':{'non_field_errors':['Emaol or passwrod is not valid']}},status=status.HTTP_404_NOT_FOUND)
+                   
+
+
+#NOW DEFINE VIEW CLASS FOR THE PRODUCT MANAGEMENT
+class ProductView(APIView):
+    def get(self,request,pk=None,format=None):
+        id=pk
+        if id is not None:
+            pro=Products.objects.get(id=id)
+            serializer=ProductsSerializers(pro)
+            return Response(serializer.data)
+        pro=Products.objects.all()
+        serializer=ProductsSerializers(pro,many=True)
+        return Response(serializer.data)
+    
+
+    def post(self,request,format=None):
+        serializer=ProductsSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Created'},status=Stats.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk,fromat=None):
+            id=pk
+            pro=Products.objects.get(id=id)
+            serializer=ProductsSerializers(pro,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg':'Complete Data Updated'})
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self,request,pk,format=None):
+        id=pk
+        pro=Products.objects.get(id=id)
+        serializer=ProductsSerializers(pro,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Partial Data Updated'})
+        return Response(serializer.errors)
+
+    def delete(self,request,pk,format=None):
+         id=pk
+         pro=Products.objects.get(id=id)
+         pro.delete()
+         return Response({'msg':'Data Deleted'})
+        
+
+
+
+#CATEGORY VIEW 
+class CategoryView(APIView):
+    def get(self,request,pk=None,format=None):
+        id=pk
+        if id is not None:
+            pro=Category.objects.get(id=id)
+            serializer=CategorySerializers(pro)
+            return Response(serializer.data)
+        pro=Category.objects.all()
+        serializer=CategorySerializers(pro,many=True)
+        return Response(serializer.data)
